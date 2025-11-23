0450d90c41d513707fd73c1a6358b1406ab39835
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-23 18:05:31 +0200

    Add project files.

---
+++ MauiUsersApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiUsersApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiUsersApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiUsersApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
