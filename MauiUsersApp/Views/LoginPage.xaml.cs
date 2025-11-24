a1f51ee1e19351db63760fc5daa7170bb5b413a3
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-24 18:08:19 +0200

    Add labels to display input validation feedback

--- MauiUsersApp/Views/LoginPage.xaml.cs
+++ MauiUsersApp/Views/LoginPage.xaml.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace MauiUsersApp.Views;
@@ -16,6 +14,8 @@ public partial class LoginPage : ContentPage
             email = value;
             OnPropertyChanged(nameof(Email));
             OnPropertyChanged(nameof(IsInputValid));
+            IsEmailErrorMessageVisible = false;
+            OnPropertyChanged(nameof(IsEmailErrorMessageVisible));
         }
     }
 
@@ -29,12 +29,62 @@ public partial class LoginPage : ContentPage
             password = value;
             OnPropertyChanged(nameof(Password));
             OnPropertyChanged(nameof(IsInputValid));
+            IsPasswordErrorMessageVisible = false;
+            OnPropertyChanged(nameof(IsPasswordErrorMessageVisible));
         }
     }
 
     public bool IsInputValid =>
         !string.IsNullOrWhiteSpace(Email) && IsValidEmail(Email) &&
         !string.IsNullOrWhiteSpace(Password);
+
+    private string emailErrorMessage;
+
+    public string EmailErrorMessage
+    {
+        get { return emailErrorMessage; }
+        set
+        {
+            emailErrorMessage = value;
+            OnPropertyChanged(nameof(EmailErrorMessage));
+        }
+    }
+
+    private bool isEmailErrorMessageVisible;
+
+    public bool IsEmailErrorMessageVisible
+    {
+        get { return isEmailErrorMessageVisible; }
+        set
+        {
+            isEmailErrorMessageVisible = value;
+            OnPropertyChanged(nameof(IsEmailErrorMessageVisible));
+        }
+    }
+
+    private bool isPasswordErrorMessageVisible;
+
+    public bool IsPasswordErrorMessageVisible
+    {
+        get { return isPasswordErrorMessageVisible; }
+        set
+        {
+            isPasswordErrorMessageVisible = value;
+            OnPropertyChanged(nameof(IsPasswordErrorMessageVisible));
+        }
+    }
+
+    private string passwordErrorMessage;
+
+    public string PasswordErrorMessage
+    {
+        get { return passwordErrorMessage; }
+        set
+        {
+            passwordErrorMessage = value;
+            OnPropertyChanged(nameof(PasswordErrorMessage));
+        }
+    }
 
     public LoginPage()
     {
@@ -45,6 +95,39 @@ public partial class LoginPage : ContentPage
     private async void Login_Clicked(object sender, EventArgs e)
     {
         await Application.Current.MainPage.DisplayAlert("Info", $"Login Button Clicked.\nEmail = {Email}\nPassword={Password}", "OK");
+    }
+
+    private void EmailEntry_Unfocused(object sender, FocusEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Email))
+        {
+            EmailErrorMessage = "Email is Required!";
+            IsEmailErrorMessageVisible = true;
+            return;
+        }
+
+        if (!IsValidEmail(Email))
+        {
+            EmailErrorMessage = "Invalid Email!";
+            IsEmailErrorMessageVisible = true;
+            return;
+        }
+
+        EmailErrorMessage = string.Empty;
+        IsEmailErrorMessageVisible = false;
+    }
+
+    private void PasswordEntry_Unfocused(object sender, FocusEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Password))
+        {
+            PasswordErrorMessage = "Password is Required!";
+            IsPasswordErrorMessageVisible = true;
+            return;
+        }
+
+        PasswordErrorMessage = string.Empty;
+        IsPasswordErrorMessageVisible = false;
     }
 
     private bool IsValidEmail(string email)
